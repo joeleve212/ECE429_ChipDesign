19b2bfa42bc6396a8ef3c1fa575d7f788ebf6626
Joe Leveille <joeleve212@gmail.com>
2020-04-03 00:20:00 -0500

    Script default commit from desktop

--- startElectric.sh
+++ startElectric.sh
@@ -2,3 +2,8 @@ cd C:/Users/Joe2TB/Documents/ECE429_ChipDesign
 git pull
 cd C:/Program\ Files/Electric
 java -jar ./electric-9.07.jar
+cd C:/Users/Joe2TB/Documents/ECE429_ChipDesign
+git add .
+git commit -m "Script default commit from desktop"
+git push
+exit
