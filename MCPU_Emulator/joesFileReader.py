b1608f51c40265976eafb13d8d279080caf647a9
Joe Leveille <joeleve212@gmail.com>
2020-04-29 14:04:38 -0500

    Integrated file reader

--- MCPU_Emulator/joesFileReader.py
+++ MCPU_Emulator/joesFileReader.py
@@ -3,8 +3,8 @@ import sys
 inFile = open("exInput.txt","r")
 lines = inFile.readlines()
 
-for i in range(len(lines)):
-    lines[i] = lines[i][0:len(lines[i])-1] #remove newline from each line
+for i in range(len(lines)):     #scroll through each line of file
+    if(i!=len(lines)-1):        #if not last line,
+        lines[i] = lines[i][0:len(lines[i])-1] #remove newline from each line
     print(lines[i])
-
 inFile.close()
