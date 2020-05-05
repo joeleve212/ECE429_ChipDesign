9ad0e763be8054167d60e1e833e942b7afc8320d
Joe Leveille <joeleve212@gmail.com>
2020-05-05 15:48:49 -0500

    commenting added & loop changed

--- MCPU_Emulator/emulator.py
+++ MCPU_Emulator/emulator.py
@@ -14,36 +14,27 @@ pc = 0
 acc = 0
 cf = 0
 
-##Possible TODO: add ways to output?
-##Possible TODO: allow for loops/more than 64 instructions?
-
-for i in range(64):
-    instruction = mem[pc & 0x3F]
-    pc += 1
-    opcode = instruction >> 6
-    arg = instruction & 0x3F
+while pc <64:                   # 
+    instruction = mem[pc & 0x3F]#using pc as the index, grab next instruction
+    pc += 1                     #increment program counter each instruction                 ----------------------------WAIT TO INCREMENT TILL AFTER PRINTING?
+    opcode = instruction >> 6   #take 2 MSBs as the op code
+    arg = instruction & 0x3F    #take the 6 LSBs as the argument
     
+    #show values of all variables at start of instruction 
     print('pc,ir,acc,cf:', pc-1, hex(instruction), acc, cf >> 8, sep='\t')
     
-    if opcode == 0:
+    if opcode == 0:         # NOR operation
         acc = (acc | mem[arg]) ^ 0xFF
-    elif opcode == 1:
+    elif opcode == 1:       # ADD operation
         acc += mem[arg]
-        cf = acc & 0x100
-        acc &= 0xFF
-    elif opcode == 2:
+        cf = acc & 0x100    #update carry flag w/ 9th bit of adder
+        acc &= 0xFF         #truncate accumulator to first 8 bits
+    elif opcode == 2:       # STA/store operation
         mem[arg] = acc
         print('+', arg, hex(mem[arg]))
-    else:
-        if instruction == 0xFF:
-            print(acc)
-        elif cf == 0:
-            pc = arg
-        cf = 0
-
-
-
-# def interpStrHex(hexVal):
-#     //
-#     //
-#     return decVal
+    else:                   # JCC operation - jump to pc = arg if carry is 0, reset carry
+        if instruction == 0xFF:    #if jumping to last loc in mem, 
+            print(acc)             #show value of accumulator                                       ----------------------WHY IS THIS A THING
+        elif cf == 0:              #if carry is 0,
+            pc = arg               #move program counter to desired location
+        cf = 0                     #reset carry
